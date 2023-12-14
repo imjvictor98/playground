25d2c3c9a3d649f37eb7753faaf0dd282babea39
João Victor Oliveira Da Câmara <joao.camara@akross.com.br>
2023-12-14 13:45:32 -0300

    Tela de carregamento no jetpack compose

--- app/src/main/java/br/com/cvj/playground/ui/home/HomeScreen.kt
+++ app/src/main/java/br/com/cvj/playground/ui/home/HomeScreen.kt
@@ -66,6 +66,7 @@ import br.com.cvj.playground.ui.search.SearchActivity
 import br.com.cvj.playground.ui.theme.Colors
 import br.com.cvj.playground.ui.theme.hirukoProFamily
 import br.com.cvj.playground.ui.theme.mazzardDmFamily
+import br.com.cvj.playground.ui.widget.LoadingCarousel
 import br.com.cvj.playground.ui.widget.atom.TextTabAtom
 import br.com.cvj.playground.ui.widget.molecule.SearchableHeaderMolecule
 import br.com.cvj.playground.ui.widget.molecule.TabLayoutMolecule
@@ -120,8 +121,8 @@ fun HomeScreen(
     when (val state = homeUiState.value) {
         is HomeUiState.IsLoading -> {
             if (state.isLoading) {
-                Box(contentAlignment = Alignment.Center) {
-                    CircularProgressIndicator(color = Colors.Salmon, strokeWidth = 5.dp)
+                Box(modifier = Modifier, contentAlignment = Alignment.Center) {
+                    LoadingCarousel()
                 }
             }
         }
