3f23a352e908036d54cf168bcade61a325f12142
João Victor Oliveira Da Câmara <joao.camara@akross.com.br>
2023-12-14 13:46:17 -0300

    removendo imports

--- app/src/main/java/br/com/cvj/playground/ui/home/HomeScreen.kt
+++ app/src/main/java/br/com/cvj/playground/ui/home/HomeScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
